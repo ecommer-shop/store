63ce4ccf310a445606f852d0d9dc3ef94e7b2587
Ronnovič Rigel Toth <rigel.toth@gmail.com>
2025-07-29 20:48:36 -0500

    [main] fix: update apiHost and uri to use the correct internal production URLs in environment.docker

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -2,10 +2,10 @@ export const environment = {
     env: 'prod',
     production: true,
     // apiHost: 'http://localhost',
-    apiHost: 'https://shop-rt.up.railway.app/shop-api',
+    apiHost: 'https://shop.railway.internal/shop-api',
     apiPort: 443,
     shopApiPath: 'shop-api',
     baseHref: '/',
     tokenMethod: 'bearer',
-    uri: "https://shop-rt.up.railway.app/shop-api",
+    uri: "https://shop.railway.internal/shop-api",
 };
